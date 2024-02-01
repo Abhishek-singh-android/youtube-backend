75b94e6ffd8791605f886c01d78bca3026d09464
Abhishek Singh <as7686854@gmail.com>
2024-02-01 20:39:36 +0530

    user all route and controller completed with aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
@@ -211,7 +212,7 @@ const logout = asyncHandler(async(req,res)=>{
         req.user._id,{
             // yaha hume batana padta hai ki hume update karna kya hai toh hum mongodb ka ek operator use kar lenge jo hai set toh set leta hai ek object ki kya kya update karna hai 
             // toh yaha humne refreshToken ko undefined kar diya hai jisse woh token hamare database se delete ho jayega
-            $set:{refreshToken:undefined}
+            $unset:{refreshToken:1} // this removes the field from the document
         },
         // ab aap yaha par aur fields bhi pass kar sakte ho jaise aap ek new value le sakte ko jise aap true kar sakte ho jisse hoga yeh ki jo return mein aapko response milega usme aapko new updated value milegi agar old value milegi toh refreshToken mil jayega toh isliye new value chahiye
         {new:true}
@@ -433,10 +434,205 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
 
 })
 
+// ---------aggregation pipeline------------
+// yaha hum user ke channel ki information get kar rahe hai
+const getUserChannelProfile = asyncHandler(async(req,res)=>{
+    // jab aapko kisi channel ki profile chahiye toh aap usually uss channel ke url par jate ho jaise /chaiaurcode toh woh hume milega req.params se yani uske url se toh hume url se nikalna hai user ka username
+    const {username} = req.params;
+    // ab check karenge jo username aaya hai woh exist karta hai yaa nahi ab humne username hi kyu nikala user se kyunki username ek unique field hai toh database mein hum isi ke basis par search karenge
+    if(!username?.trim()){
+        throw new ApiError(400,"username is missing")
+    }
+
+    // ab yaha tak aa gaye toh matlab mere pass username toh hoga
+    // ab sabse pehle hum sochenge ki username se apna document find kar lete hai qeury chalate hai user.find() usme where clause apne aap lag jayega kuch iss tharah se User.find({username}) toh yeh thik hai
+    // lekin yaha par problem yeh hai ki pehle database se user loge pura fir uski id ke base basis par aggregation lagaoge itni jarurat hai nahi karne ki hum direct hi aggregation pipeline laga sakte hai
+    // kyunki aggregation pipeline mein "match" field hota hai toh woh automatically sare documents mein se ek document ko find kar lega aur yahi "match" ka kaam hai toh hum aggregation pipeline lagayenge User.aggregate se 
+    // aur aggregate ek method hai jo array leta hai aur uss array me hum diffrent diffrent pipelines likhte hai aur pipelines hum likhte hai curly braces mein 
+    // ab jo bhi aggregate se result aayega usse store kar lenge chanel variable mein aur jo aggregate pipelines se jo result aate hai woh  array ke format mein aata hai result
+    const channel = await User.aggregate([
+        // ab hum pehli pipeline likh rahe hai "match" field ki aur match field ko chahiye hota hai ki kaise match karu toh hamare pass hai username toh hum match kar lenge username se
+        {
+            $match:{
+                username: username?.toLowerCase()
+            }
+        },
+        //filter karne ke baad ab yaha tak aa gaye hai toh mere pass ek document hai sirf ab iss document ke basis par mujhe karna hai "lookup" matlab ab mere paas ek user hai aur uske mujhe batane hai ki kitne subscriber hai yeh mujhe find karna hai
+        // toh subscribers find karne ke liye "lookup" lagega toh ab yeh lookup wali ban jayegi hamari dusri pipeline ab "lookup" ko chahiye hote hai kuch fields ab sabse pehla field hai "from" matlab kanha se dekhna chahte ho
+        // toh mein subscription model mein ja kar dekh leta hun toh mein subscription model mein ja kar bol deta hun mujhe chahiye subscriber toh humne "Subscription" naam se banaya tha collection mongodb mein toh "from" mein aayega "subscription" ki issi collection mein se dekho
+        {
+            $lookup:{
+                from:"subscriptions",
+                // ab dusri field "lookup" ko chahiye hoti hai "localfield" toh localfield laga lete hai "_id" ki id mein se dekhne hai
+                localField:"_id",
+                // ab third field hai ki kanha par hoga yeh yeh foreign field present ab kyunki hume subscriber chahiye toh uske channel ko select kar lenge aur agar channel chahiye hota toh subscriber ko select kar lete 
+                foreignField:"channel",
+                // ab last field mein hum batayenge ki hume isse kis "lookup" ko kis naam se bolna hai 
+                as:"subscribers"
+                // ab yaha tak hamare channel mein sare subscriber find ko gaye hai lekin abhi humne un subscribers ka count nahi nikala hai
+                            
+            }
+            // ab yaha tak humne pata kar liya ki kitne subscriber hai mere iss particular channel par
+        },
+
+        // ab hum third pipeline likhte hai
+        // ab hume yeh pata karna hai ki meine kitne subscribe kiye hai channels toh woh bhi toh nikalna hai pehle toh humne sirf apne channel ke subscriber nikale ki mere channel par kitne subscriber hai lekin mujhe yeh bhi toh pata karna hai ki meine kitne channels ko subscribe kiya hai
+        // toh hum fir se "lookup" ka use karenge aur "from" same hi rahega kyunki "subscription" collection ke ander hai "channel" ki details
+        // ab "foreign" field ke ander aayega "subscriber" toh mujhe woh saare channels mil jayenge jisko meine subscibe kiya hai 
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                // ab "as" field aayega ki kis kis ko subscibe kiya hai toh iska naam hum rakh lete hai "subscibedTo"
+                as:"subscribedTo"
+            }
+        },
+        // ab hamare pass lookup mein yeh dono fields aa gaye hai matlab jo do pipelines likhi hai "lookup" ki toh inhe add bhi karna padega kyunki yeh dono alag alag hai 
+        // toh hum ek aur pipeline bana lenge toh ek aur operator hota hai "addFields" jo yeh karta ki jitne bhi hamare schema mein fields hai woh toh rakhega hi sath hi kuch additional fields add kar dega aur yahi toh hume karna hai ki ek hi object mein hum sara ka sara data bhej de
+        {
+            $addFields:{
+                // toh additional field meine hume kya kya add karna hai toh pehle hume add karna hai "subscribersCount" ab subscribersCount calculate kaise hoga toh abhi tak humne pipelines mein saare ducuments ko collect kar ke rakh liya hai
+                // ab hamara jo field hai "subscribers" isse hume calculate karna padega toh agar hume sare documents calculate karna hai count karna hai ki kitne hai toh hamare pass ek "size" naam se hota hai
+                // jismein hume batana hota hai ki kis field meine se hume calculate karna hai toh hume calculate karna hai "subscribers" field mein se aur kyunki yeh ek field hai toh iske aage hum $ sign laga denge kuch aise "$subscribers"
+                subscribersCount:{
+                    $size:"$subscribers",
+                },   
+                // ab hume ek aur field add karna hai "subscribedTo" ke count ke liye toh uska naam hum rakh lete hai "channelsSubscribedToCount"
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+             // ab hume ek field aur banani padegi yeh show karne ke liye ki user ne channel ko subscribe kiya hai ya nahi 
+             // ab yaha hume rakhenge ek condition aur condittion mein usually three parameters hote hai "if" janha par aap condition likhte ho dusra parameter hota hai "then" agar true hai toh then nahi toh else 
+             isSubscribed: {
+                $cond:{
+                    // ab "if" mein hum condition denge ki aapke pass jo document aaya "subscribers" usmein mein hu ki nahi toh already woh document mere pass hai toh kaise calculate karte hai toh ek aur operator mere pass hai "in" jo calculate karke de deta hai aapko array aur object dono ke ander
+                    // toh hume kya check karna hai toh agar aap already loggedIn honge toh req.user aapke pass hoga toh req.user mein se id nikal lo aur yeh bhi bata denge ki kis object mein se dekhna hai toh 
+                    // harame pass yeh "subscribers" hai already aur $ sign laga denge yeh batane ke liye ki yeh ek field hai toh yaha dekh lenge ki subscribers mein subscriber hai ya nahi
+                    $if:{$in: [req.user?._id,"$subscribers.subscribe"]},
+                    // ab agar yeh aaya true matlab subscribed hai tabhi toh true mila hai subscibe mein "$in" mein
+                    then:true,
+                    else:false          
+                }
+             }
+            }
+        },
+        //ab mujhe ek aur pipeline use karni hai jo hai "project" ab "project" kya hota hai projection deta hai ki mein sari values ko nahi ekdum project karuga jo bhi usko demand kar raha hai
+        // mein usko selected cheeje dunga ab selected cheeje kaise dunga ab selected cheeje ke ander aapko jis jis chejo ko pass on karna hai uske aage laga dunga 1 aur woh value pass ho jayegi
+        {
+            $project:{
+                fullName:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+       
+    ])
+
+       // ab hume dekhna hai ki aggregate pipeline use karne ke baad uske result mein kya aa raha hai 
+        // toh output mein hume array milta hai jisme multiple values ho sakti hai lekin hamare case mein array mein ek hi value hogi kyunki match ke ander final ek hi user hai toh ek hi user ka profile milega
+       
+        // ab hum pehle check kar lete hai ki channel mein kuch hai bhi ya nahi
+
+        if(!channel?.length){
+            throw new ApiError(400,"channel does not exist")
+        }
+
+        // ab return mein hum array mein se jo values hai woh nikal kar front end mein sidi woh values hi bhej denge kyunki ek hi toh object hai
+        return res
+        .status(200)
+        .json(new ApiResponse(200,channel[0],"User channel fetched successfully"))
+
+})
+
+
+// ab hume lana hai watchHistory toh jaise hi user click karega watchHistory par toh saare videos ka list aa jayega jo videos user ne view kar rakhe hai toh watchHistory lane ke liye hume join karna padega toh join kisse karna padega 
+// toh join kisse karna padega ki yeh watchHistory hamare pass array tha jisme sare videos ki id hum add karte ja rahe the toh hamare pass bahut sari ids mil jayegi toh bahut sari hum log ids le aayenge toh bahut sare documents hume mil jayenge
+// toh watchHistory se jaise hi mein lookup karunga videos mein toh bahut sare ducument watchHistory ke document ke ander mere pass aa jayenge aur mujhe mil jayenge lekin ek lookup karke toh mujhe videos ke bahut sare document mil jayenge lekin us video model mein owner bhi toh hai
+// toh hume owner bhi chahiye video ka aur owner wapas se user hai matlab owner user model se hi hai na toh ek lookup karne se toh aapko watchHistory ke ander aapko sare videos mil jayenge lekin hume ek aur nested lookup karna padega toh further nested lookup isliye karna padega kyunki 
+// further down ek lookup se toh hamare pass videos ke document aa jayenge lekin hum usko chain nahi kar sakte further down toh isi object ke ander (video model) mujhe owner ke ander further ek aur lookup karna padega toh har ek document ke ander ek aur join hona chahiye toh isiliye mujhe nested lookup karna padega
+// toh nested lookup mein hoga ki owner se jao user ke pass aur uski jo jo information aapko chahiye woh hum lekar aayenge aur populate kar lenge usko ki kon kon si information chahiye usko kyunki owner ke ander milega toh pura user object hi lekin usmein se kitna further chahiye woh hum pick kar lenge
+// toh watch history se nested lookup isliye kar rahe hai kyunki watch history se jaise hi join karenge humko multiple ducuments milenge lekin un documents ke ander aapke pass owner hoga hi nahi toh isilye hum kya karenge ki jaise hi mujhe woh document mila further ek aur usi time par lookup ya join kar denge taki mujhe mujhe woh perfect document mile 
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+    // toh req mein jo _id hum pass karte hai woh string hi pass karte hai hum kyunki mongodb ki id toh hoti hai ObejctId('272828jdhdhd') kuch is type se Object id ke format mein toh mongoose mein kya hota hai internally ki 
+    // hum usko _id dete hai jo string hi hoti hai aur mongoose automatically behind the schene usko convert kar deta hai mongoDb ki object id mein 
+    // ab aggregation pipeline hum direct _id MongoDb ki id mein convert nahi hoti hai kyunki yanha moongoose usse automatically convert nahi kar pata toh hume hi convert karna padta hai usse
+    
+    const user = await User.aggregate([
+      // first pipeline
+        {
+            // toh yaha match se hume user mil jayega jo apni watchHistory check karna chahta hai
+            $match:{
+                // ab yaha mongoose kaam nahi karta hai sahi se automatically id convert katne mein kyunki aggregation pipeline ka jo code hai woh directly hi jata hai toh yaha hum use le lenge mongoose ki ObjectId property ka
+                _id: new mongoose.Types.ObjectId(req.user._id) 
+            },
+            // ab hume user ki watchHistory ke ander jana padega toh hume lookup karna padega toh lookup karte hai 
+            // second pipeline
+           
+        },
+
+        {
+            // ab lookup hum lagayenge "videos" model par kyunki saari videos video model se hi toh ban rahi hai toh video model ka collection bana hai "videos" naam se isliye from mein "videos" aayega
+            $lookup:{
+                from:"videos",
+                // ab localField hai "watchHistory" kyunki yahi hume pata karna hai user model mein
+                localField:"watchHistory",
+                // ab foreign field mein hum _id rakh lete hai
+                foreignField:"_id",
+                as:"watchHistory",
+                // ab yaha tak hamare pass sari watchHistory ki videos ka data aa gaya hai lekin ab hume ek sub pipeline lagani padegi warna hume iss owner field mein kuch nahi milega ki kis channel ki video hai yeh
+                // toh woh nested pipeline lagane ke liye aap lookup ke sath comma lagane ke baad use kar sakte ho pipeline aur further down aapko jitni pipelines chahiye aap laga sakte ho 
+                 pipeline:[
+                    {
+                        $lookup:{
+                            // ab hume owner ka data chahiye toh woh milega user model mein isliye from mein "users" aayega 
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            // ab yaha owner field ke ander user ka bahut sara data aa gaya hai jisme se kuch unnecessary bhi hai toh hum select kar sakte hai ki hume aage kon kon sa data pass karna hai
+                            // toh hum fir se ek nested pipeline ka use kar sakte hai
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+
+                        }
+                    },
+
+                // ab yeh pipeline hum bana rahe hai taki hum front end par complex nahi easy data bheje kyunki saara data owner mein hai aur woh bhi array format mein toh front end dev ko array mein se fields nikalni padegi usse accha hum hi array se fields nikal kar direct frontend ko pass kar de
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                 ]     
+            }
+            
+        }
+
+
+    ])
+
+    return res.status(200)
+    .json( new ApiResponse(200,user[0].watchHistory,"Watch history fetched successfully!"))
+})
 
 
 
 
-
-export {registerUser,loginUser,logout,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage}
+export {registerUser,loginUser,logout,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails,updateUserAvatar,updateUserCoverImage,getUserChannelProfile,getWatchHistory}
 
--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logout, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logout, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -36,5 +36,26 @@ router.route("/logout").post(verifyJWT,logout)
 
 // ab yaha hum woh route bana rahe hai jab user ka accessToken expire ho jata hai aur new token user ko dena hota hai
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+// ab avatar image ko update karane wale hai toh pehle middleware aayega verifyJWT ka for checking logged in user aur dusra middleware aayega multer ka for uploading files
+// aur kyunki hum ek hi file upload kara rahe hai isliye .single() method use kiya hai waise ismein array wagerah aur bhi methods hote hai isliye controller mein files nahi file aa raha hai
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/coverImage").patch(verifyJWT,upload.single("avatar"),updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watchHistory").get(verifyJWT,getWatchHistory)
+
+
+
+
+
 
 export default router;
